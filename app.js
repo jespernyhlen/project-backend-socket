cc6d435808efa407aba67b83a65e84ff27470be0
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-10-23 19:05:11 +0200

    Trying to get origins to work

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.origins(['https://project-frontend.jespernyhlenjs.me:443']);
+io.origins(['https://project-frontend.jespernyhlenjs.me/chart:443']);
 
 var stone = {
     name: 'stone',
